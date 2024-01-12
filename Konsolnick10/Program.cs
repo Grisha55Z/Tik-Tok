be98a21e8407be1f444529bb82e32a9ab62f08d0
student <student@CLASS-10-05>
2024-01-12 14:28:40 +0500

    first commit

---
+++ Konsolnick10/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Konsolnick10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+        }
+    }
+}
